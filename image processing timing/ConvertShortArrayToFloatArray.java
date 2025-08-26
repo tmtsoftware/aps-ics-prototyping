62edfd3d9be674c4867e0d481e5a0d0b62445bdb
Angelic Ebbers <aebbers@tmt.org>
2025-08-25 15:34:20 -1000

    added burn-in trials and command line arguments to facilitate updating benchmarks.

--- image processing timing/ConvertShortArrayToFloatArray.java
+++ image processing timing/ConvertShortArrayToFloatArray.java
@@ -1,6 +1,32 @@
 public class ConvertShortArrayToFloatArray {
+    private static final int BURN_IN_TRIALS = 3;
     public static void main(String[] args) {
-        int arraySize = 8120 * 8120;
+        int width = 8120;
+        int height = 8120;
+        int iterations = 100;
+        // Parse arguments
+        if (args.length >= 2) {
+            try {
+                width = Integer.parseInt(args[0]);
+                height = Integer.parseInt(args[1]);
+            } catch (NumberFormatException e) {
+                System.err.println("Invalid width/height, using default 8120 x 8120.");
+                width = 8120;
+                height = 8120;
+            }
+        }
+
+        if (args.length >= 3) {
+            try {
+                iterations = Integer.parseInt(args[2]);
+            } catch (NumberFormatException e) {
+                System.err.println("Invalid iteration count, using default of 100.");
+                iterations = 100;
+            }
+        }
+
+        int arraySize = width * height;
+        System.out.printf("Running conversion benchmark with arraySize=%d, iterations=%d%n", arraySize, iterations);
 
         short[] shortArray = new short[arraySize];
         // Initialize your short array here...
@@ -11,7 +37,7 @@ public class ConvertShortArrayToFloatArray {
         double max = 0.0;
         double totalTimeInMilliseconds = 0;
         
-        for (int i=0; i<100; i++) {
+        for (int i=0; i< iterations + BURN_IN_TRIALS; i++) {
             
             long startTime = System.nanoTime();
 
@@ -20,11 +46,13 @@ public class ConvertShortArrayToFloatArray {
             long endTime = System.nanoTime();
 
             double deltaTime = (endTime - startTime) / 1_000_000;
-            totalTimeInMilliseconds += deltaTime;
-            max = (max < deltaTime) ? deltaTime : max;
+            if (i >= BURN_IN_TRIALS) {
+                totalTimeInMilliseconds += deltaTime;
+                if (deltaTime > max) max = deltaTime;
+            }
          }
 
-        System.out.println("Average Conversion time 100 trials: " + totalTimeInMilliseconds/100.0 + " milliseconds,   max time = " + max);
+        System.out.println("Average Conversion time 100 trials: " + totalTimeInMilliseconds/iterations + " milliseconds,   max time = " + max);
     }
 
     private static void convertShortArrayToFloatArray(short[] shortArray, float[] floatArray) {
